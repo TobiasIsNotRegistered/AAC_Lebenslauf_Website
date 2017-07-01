8f8df7f1b97580886aac3972eff3c4cb8b7ceb85
Tobias Sigel <tobias.sigel@students.fhnw.ch>
2017-07-01 17:20:38 +0200

    added animated hamburger button, some styling.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,4 @@
+/*
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
@@ -9,3 +10,5 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
 }
+
+*/
